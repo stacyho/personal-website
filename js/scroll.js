9fa84fda6650f8b24a8e235e660092738be2ca46
Stacy Ho <stacy_ho@yahoo.com>
2016-09-08 23:56:32 -0400

    new layout

--- js/scroll.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("#arrow-down").on('click', function(event){
-    event.preventDefault();
-    $('html, body').animate({
-        scrollTop: $(window).height()
-    }, 750);
-  });
-});
